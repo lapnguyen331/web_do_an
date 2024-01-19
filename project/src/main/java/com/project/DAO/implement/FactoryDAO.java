9beb23f30f7dbe2127df34a2e0c7d19f886ea6f5
Nguyen Tuan <nqat0919@gmail.com>
2024-01-19 19:11:26 +0700

    Issue #33: Small fixes

--- project/src/main/java/com/project/DAO/implement/FactoryDAO.java
+++ project/src/main/java/com/project/DAO/implement/FactoryDAO.java
@@ -1,9 +1,6 @@
 package com.project.dao.implement;
 
-import com.project.dao.IBlogDAO;
-import com.project.dao.ICategoryDAO;
-import com.project.dao.IImageDAO;
-import com.project.dao.IProductDAO;
+import com.project.dao.*;
 import com.project.db.JDBIConnector;
 import org.jdbi.v3.core.Handle;
 
@@ -12,11 +9,13 @@ public abstract class FactoryDAO {
     public static final Class<IImageDAO> DAO_IMAGE = IImageDAO.class;
     public static final Class<IBlogDAO> DAO_BLOG = IBlogDAO.class;
     public static final Class<ICategoryDAO> DAO_CATEGORY = ICategoryDAO.class;
+    public static final Class<IUserDAO> DAO_USER = IUserDAO.class;
     public static <T> T getDAO(Handle handle, Class<?> clazz) {
         if (clazz == DAO_PRODUCT) return (T) new ProductDAO(handle);
         else if (clazz == DAO_IMAGE) return (T) new ImageDAO(handle);
         else if (clazz == DAO_BLOG) return (T) new BlogDAO(handle);
         else if (clazz == DAO_CATEGORY) return (T) new CategoryDAO(handle);
+        else if (clazz == DAO_USER) return (T) new UserDAO(handle);
         return null;
     }
 }
